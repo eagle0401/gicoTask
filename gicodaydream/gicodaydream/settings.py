9c8f5b7a5ba8325ea843a5664e6a85bd12221551
denny_hsiao <denny_hsiao@asus.com>
2018-05-03 15:36:51 +0800

    Add readme and staticfiles_dirs

--- gicodaydream/gicodaydream/settings.py
+++ gicodaydream/gicodaydream/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__), '..', 'static'),
+)
